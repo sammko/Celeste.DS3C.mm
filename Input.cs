08fde29cfc1ee3669d321cf48cd31f91c54ef867
Samuel Čavoj <sammko@sammserver.com>
2018-02-25 19:26:01 +0100

    Initial commit.

---
+++ Input.cs
@@ -0,0 +1,30 @@
+﻿using Monocle;
+using Microsoft.Xna.Framework.Input;
+
+namespace Celeste
+{
+    public static class patch_Input
+    {
+        public static string GuiInputPrefix() {
+            if (!string.IsNullOrEmpty(Input.OverrideInputPrefix))
+            {
+                return Input.OverrideInputPrefix;
+            }
+            if (!MInput.GamePads[Input.Gamepad].Attached)
+            {
+                return "keyboard";
+            }
+            string guid = GamePad.GetGUIDEXT(MInput.GamePads[Input.Gamepad].PlayerIndex);
+            if (guid.Equals("4c05c405") || guid.Equals("4c05cc09")
+                || guid.Equals("4c056802"))
+            {
+                return "ps4";
+            }
+            if (guid.Equals("7e050920") || guid.Equals("7e053003"))
+            {
+                return "ns";
+            }
+            return "xb1";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Celeste patch, use [MonoMod](https://github.com/0x0ade/MonoMod) to apply.
+Makes the game use PlayStation button icons when using a Dualshock 3 (PS3) controller.
